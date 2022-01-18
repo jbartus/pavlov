2d311b798a26dc38630e8afb891f243b742114c6
Jim Bartus <jim.bartus@gmail.com>
2022-01-17 23:17:21 -0500

    iam policy for lambda

--- main.tf
+++ main.tf
@@ -165,4 +165,24 @@ resource "aws_iam_role" "iam_for_lambda" {
     ]
   })
 
+  inline_policy {
+    name = "logsandddb"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+          Effect   = "Allow"
+          Resource = "arn:aws:logs:*:*:*"
+        },
+        {
+          Action   = ["dynamodb:PutItem", "dynamodb:GetItem"]
+          Effect   = "Allow"
+          Resource = "${aws_dynamodb_table.pavlov-servers.arn}"
+        }
+      ]
+    })
+  }
+
 }
