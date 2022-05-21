39f4e9628590ec2587245f892c3be3011f0f44ce
Jim Bartus <jim.bartus@gmail.com>
2022-05-20 23:47:34 -0400

    improve modeling/validating

--- function/index.py
+++ function/index.py
@@ -3,7 +3,8 @@ import boto3
 import os
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Union
 from mangum import Mangum
 from dotenv import load_dotenv
 from pathlib import Path
@@ -14,19 +15,19 @@ class PavlovServer(BaseModel):
     """
     This class is used to define the server's configuration.
     """
-    #bEnabled: bool
-    ServerName: str
-    #MaxPlayers: int
-    #ApiKey: Union[str, None] = None
-    #bSecured: bool
-    #bCustomServer: bool
-    #bVerboseLogging: bool
-    #bCompetitive: bool
-    #bWhitelist: bool
-    #RefreshListTime: int
-    #LimitedAmmoType: int
-    #TickRate: int
-    #TimeLimit: int
+    bEnabled: bool = True
+    ServerName: str = Field(min_length=5, max_length=35)
+    MaxPlayers: int = Field(default=10, gt=0, lt=25)
+    ApiKey: Union[str, None] = None
+    bSecured: bool = True
+    bCustomServer: bool = True
+    bVerboseLogging: bool = False
+    bCompetitive: bool = False
+    bWhitelist: bool = False
+    RefreshListTime: int = Field(default=120, gt=0)
+    LimitedAmmoType: int = Field(default=0, lt=6)
+    TickRate: int = Field(default=90, gt=29, lt=241)
+    TimeLimit: int = Field(default=60)
     #Password=0000
     #BalanceTableURL="vankruptgames/BalancingTable/main"
     #MapRotation=(MapId="sand", GameMode="DM")
